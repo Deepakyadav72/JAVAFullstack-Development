64ad4025297333a2a5a4e5922a71b1a2eb9c25d0
Deepakyadav72 <deepakyadav723497@gmail.com>
2025-08-18 12:02:46 +0530

    js

---
+++ js/data-types.js
@@ -0,0 +1,12 @@
+var_str='string';
+var str="string";
+var str=`string`;
+
+var num =19;
+var bool =true;
+var arr =[1,2,3,4];
+var arrofObject=[{str:'name'},{number:2}];
+var obj={name:'Deepak',age:26};
+var und=undefined;
+var nul=null;
+var sym=symbol();
