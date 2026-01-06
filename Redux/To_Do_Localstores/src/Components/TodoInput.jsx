6f64f4791f61c8668e6023e33e52329412bf4fc0
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-01-06 17:22:01 +0530

    todo

---
+++ Redux/To_Do_Localstores/src/Components/TodoInput.jsx
@@ -0,0 +1,18 @@
+import React , {useRef} from 'react';
+export const TodoInput=()=>{
+    const valueData= useRef(null);
+
+    const handleAdd=()=>{
+        const valueText=inputData.current.value;
+       
+        console.log(valueText);
+
+    };
+    return(
+        <>
+        <h1>Todo</h1>
+        <input type="text" ref={inputData} />
+        <button onClick={()=>handleAdd}>add</button>
+        </>
+    );
+};
