3f34355ac27b0eb51d739253a71c60c31bca20d7
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-01-08 20:57:32 +0530

    Todo-confirm

---
+++ Redux/local_redux/src/Reducer/Todos/Reducer.js
@@ -0,0 +1,44 @@
+import * as types from './Actions'
+
+const initialValue = { todo: [ ] };
+
+
+export const todoReducer = (state = initialValue,{ type, payload }) =>{
+    switch ( type ){
+        case types.ADDTODOS:{
+            if(payload.trim() === ''){
+                return state;
+            } else {
+                const todoValue = {
+                    id:Date.now(),
+                    text:payload,
+                    isEdit:false,
+                    isComplete:false
+                };
+                return {
+                    ...state,
+                    todo: [...state.todo, todoValue]
+                };
+            }
+        }
+
+
+        case types.DELETETODOS:{
+            return {
+                ...state,
+                todo: state.todo.filter((el)=> el.id !== payload)
+            }
+        }
+      
+       
+             
+
+
+
+
+
+
+        default:
+            return state;
+    }
+}
---
+++ Redux/local_redux/src/Router/AllRoutes_routes.js
@@ -0,0 +1,22 @@
+import { Route, Routes} from  'react-routes-dom';
+
+import React  from'react';
+import {Home} from '../Pages/Home';
+import {Todo} from '../Pages/Todo';
+import {Signup} from '../Pages/Singnup';
+import {Login} from '../Pages/Login';
+import {Counter} from '../components/counter';
+
+
+export const AllRoutes=()=>{
+    return(
+        <Routes>
+            <Routes path="/" element={<Home/>}></Routes>
+             <Routes path="/login" element={<Login/>}></Routes>
+              <Routes path="/signup" element={<Signup/>}></Routes>
+               <Routes path="/todo" element={<Todo/>}></Routes>
+                <Routes path="/counter" element={<Counter/>}></Routes>
+        </Routes>
+    );
+
+};
