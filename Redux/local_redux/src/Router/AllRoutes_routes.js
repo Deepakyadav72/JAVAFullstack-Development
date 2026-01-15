c39023a936c682e8a84624f7fbe12cd7f54e3851
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-01-15 17:09:33 +0530

    solo

--- Redux/local_redux/src/Router/AllRoutes_routes.js
+++ Redux/local_redux/src/Router/AllRoutes_routes.js
@@ -10,13 +10,22 @@ import {Counter} from '../components/counter';
 
 export const AllRoutes=()=>{
     return(
+        <>
+        <Navbar/>
         <Routes>
             <Routes path="/" element={<Home/>}></Routes>
              <Routes path="/login" element={<Login/>}></Routes>
               <Routes path="/signup" element={<Signup/>}></Routes>
-               <Routes path="/todo" element={<Todo/>}></Routes>
+               <Routes path="/todo" 
+               element={
+                <PrivateRoutes>
+               <Todo/>
+               </PrivateRoutes>
+               
+               }></Routes>
                 <Routes path="/counter" element={<Counter/>}></Routes>
         </Routes>
+        </>
     );
 
 };
