82d8e5f55c5300bd61d54e241f18ea009826bbd3
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-01-30 23:17:22 +0530

    day of code

--- Redux/coustomHook/src/App.jsx
+++ Redux/coustomHook/src/App.jsx
@@ -1,61 +1,61 @@
-// import { useState } from "react";
-// import './App.css'
-// import { useLocalStorage } from "./hooks/useLocalStorage";
-// import { useToggle } from "./hooks/useToggle";
-
-
-// function App() {
-//   const[string, setString]=useState('');
-//   const [value ,setValue]=useLocalStorage('name',[]);
-  
-//   const[toggle, setToggle]=useToggle();
-//   console.log('🚀 ~ toggle:', toggle);
-
-//   return (
-//     <>
-//     <input type="text" onChange={(e)=> setString(e.target.value)}/>
-//     <button onClick={()=>setValue(string)}>click</button>
-//     {toggle && <h1>i am invoke</h1>}
-//     <button onClick={()=>setToggle(false)}>off</button>
-//     <button onClick={()=>setToggle(true)}>on</button>
-
-//       </>
-//   );
-// }
-
-// export { App};
-
-
 import { useState } from "react";
 import './App.css'
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import { useToggle } from "./hooks/useToggle";
 
-function App() {
-  const [string, setString] = useState('');
-  const [value, setValue] = useLocalStorage('name', []);
 
-  const [toggle, setToggle] = useToggle();
+function App() {
+  const[string, setString]=useState('');
+  const [value ,setValue]=useLocalStorage('name',[]);
+  
+  const[toggle, setToggle]=useToggle();
+  console.log('🚀 ~ toggle:', toggle);
 
   return (
     <>
-      <input
-        type="text"
-        onChange={(e) => setString(e.target.value)}
-      />
+    <input type="text" onChange={(e)=> setString(e.target.value)}/>
+    <button onClick={()=>setValue(string)}>click</button>
+    {toggle && <h1>i am invoke</h1>}
+    <button onClick={()=>setToggle(false)}>off</button>
+    <button onClick={()=>setToggle(true)}>on</button>
 
-      <button onClick={() => setValue(string)}>
-        click
-      </button>
-
-      {toggle && <h1>I am invoke</h1>}
-
-      {/* Single Toggle Button */}
-      <button onClick={() => setToggle(!toggle)}>
-        {toggle ? "OFF" : "ON"}
-      </button>
-    </>
+      </>
   );
 }
 
-export { App };
+export { App};
+
+
+// import { useState } from "react";
+// import './App.css'
+// import { useLocalStorage } from "./hooks/useLocalStorage";
+// import { useToggle } from "./hooks/useToggle";
+
+// function App() {
+//   const [string, setString] = useState('');
+//   const [value, setValue] = useLocalStorage('name', []);
+
+//   const [toggle, setToggle] = useToggle();
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         onChange={(e) => setString(e.target.value)}
+//       />
+
+//       <button onClick={() => setValue(string)}>
+//         click
+//       </button>
+
+//       {toggle && <h1>I am invoke</h1>}
+
+//       {/* Single Toggle Button */}
+//       <button onClick={() => setToggle(!toggle)}>
+//         {toggle ? "OFF" : "ON"}
+//       </button>
+//     </>
+//   );
+// }
+
+// export { App };
