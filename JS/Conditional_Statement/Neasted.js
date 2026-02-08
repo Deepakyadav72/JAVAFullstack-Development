0ff997143057dd62b33ac84c8ed652880c5a1743
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-02-08 13:51:22 +0530

    neasted

---
+++ JS/Conditional_Statement/Neasted.js
@@ -0,0 +1,13 @@
+let Age=18;
+let license=true;
+
+if(Age>=20)
+{
+    if(license=false){
+        console.log("You can drive");
+    }else{
+        console.log("you need to make the license");
+    }
+}else{
+    console.log("you are not able to drive");
+}
