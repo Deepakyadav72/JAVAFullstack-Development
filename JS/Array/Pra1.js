fba760162d3ad9f6246766e7511dd6100e6b0b41
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-03-06 16:51:57 +0530

    music

--- JS/Array/Pra1.js
+++ JS/Array/Pra1.js
@@ -1,17 +1,21 @@
-const city=["mumbai","Delhi","Pune"]
+// const city=["mumbai","Delhi","Pune"]
 
-console.log(city);
-
-
+// console.log(city);
 
 
-const city1= new Array();
-city1[0]="mumbai";
-city1[1]="jaunpur";
-city1[2]="pune";
-
-console.log(city1[1]);
 
 
-const city2= new Array("deepa","manoj","vikas");
-console.log(city2[2]);
+// const city1= new Array();
+// city1[0]="mumbai";
+// city1[1]="jaunpur";
+// city1[2]="pune";
+
+// console.log(city1[1]);
+
+
+// const city2= new Array("deepa","manoj","vikas");
+// console.log(city2[2]);
+
+var arr=[29,23,45,23];
+arr.reverse();
+console.log(arr);
