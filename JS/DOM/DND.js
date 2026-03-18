b1643d4a97cf50460ada7260f037c56d30202ee6
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-03-18 23:48:30 +0530

    DOM

---
+++ JS/DOM/DND.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',function () {
+ const body = document.body;
+ const content =document.getElementById('content');
+ const toggleBtn =document.getElementById('toggleBtn');
+ let nightMode = false;
+ toggleBtn.addEventListener('click', function () {
+ nightMode = !nightMode;
+ if (nightMode) {
+ body.classList.add('night-mode');
+ content.style.color = '#fff';
+ } else
+     {
+      body.classList.remove('night-mode');
+ content.style.color = '#000';
+}
+ });
+});
