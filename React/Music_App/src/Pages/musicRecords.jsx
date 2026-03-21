ba3f2d7d5e7f8dc6f7c40a20ab555a097fcb5dd6
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-03-21 23:13:52 +0530

    task

--- React/Music_App/src/Pages/musicRecords.jsx
+++ React/Music_App/src/Pages/musicRecords.jsx
@@ -1,37 +1,37 @@
-// import React from "react";
-// import styled from "styled-components"
-// import { FilterMusicRecords } from "../Components/FiltermusicRecords";
-// import { MusicAlbum } from "../Components/MusicAlbum";
+import React from "react";
+import styled from "styled-components"
+import { FilterMusicRecords } from "../Components/FiltermusicRecords";
+import { MusicAlbum } from "../Components/MusicAlbum";
 
-// export const MusicRecord = () => {
-//     return(
-//         <Wrapper>
-//         <WrapperFilterMusicRecords>
-//             <FilterMusicRecords/>
-//         </WrapperFilterMusicRecords>
-//          <WrapperMusicAlbumRecords>
-//             <MusicAlbum/>
-//          </WrapperMusicAlbumRecords>
-//         </Wrapper>
-//     );
-// };
+export const MusicRecord = () => {
+    return(
+        <Wrapper>
+        <WrapperFilterMusicRecords>
+            <FilterMusicRecords/>
+        </WrapperFilterMusicRecords>
+         <WrapperMusicAlbumRecords>
+            <MusicAlbum/>
+         </WrapperMusicAlbumRecords>
+        </Wrapper>
+    );
+};
 
-// const Wrapper = styled.div`
-//   border: 1px solid red;
-//   display: flex;
-//   justify_content: center;
-//   `;
+const Wrapper = styled.div`
+  border: 1px solid red;
+  display: flex;
+  justify_content: center;
+  `;
 
-//   const WrapperFilterMusicRecords = styled.div`
-//     border: 1px solid green;
-//     width:30%;
-//     height: 100%;
-// `;
+  const WrapperFilterMusicRecords = styled.div`
+    border: 1px solid green;
+    width:30%;
+    height: 100%;
+`;
 
-// const WrapperMusicAlbumRecords = styled.div`
-//    border: 1px dashed pink;
-//    width: 70px;
-//    height:100%;
-//    display: grid;
-//    grid-template-columns:repeat(auto-fit, minmax(300px, 1fr))
-//    `;
+const WrapperMusicAlbumRecords = styled.div`
+   border: 1px dashed pink;
+   width: 70px;
+   height:100%;
+   display: grid;
+   grid-template-columns:repeat(auto-fit, minmax(300px, 1fr))
+   `;
