645cdf69e9b3251052905d557202b091bb9fc0cc
Deepakyadav72 <deepakyadav723497@gmail.com>
2026-03-27 23:00:15 +0530

    hh

--- JS/ValidationForm/script.js
+++ JS/ValidationForm/script.js
@@ -1,55 +1,55 @@
-function validation(){
+// function validation(){
 
-    document.getElementById("fnameError").innerHTML="";
-    document.getElementById("lnameError").innerHTML="";
-    document.getElementById("emailError").innerHTML="";
-    document.getElementById("passError").innerHTML="";
-    document.getElementById("genderError").innerHTML="";
-    document.getElementById("skillsError").innerHTML="";
-    document.getElementById("cityError").innerHTML="";
+//     document.getElementById("fnameError").innerHTML="";
+//     document.getElementById("lnameError").innerHTML="";
+//     document.getElementById("emailError").innerHTML="";
+//     document.getElementById("passError").innerHTML="";
+//     document.getElementById("genderError").innerHTML="";
+//     document.getElementById("skillsError").innerHTML="";
+//     document.getElementById("cityError").innerHTML="";
 
 
-    var fname= document.getElementById("fname").value;
-     var lname= document.getElementById("lname").value;
-     var email= document.getElementById("email").value;
-     var pass= document.getElementById("pass").value;
-     var gender=document.querySelector('input[name="gen"]:checked');
-    var skills=document.querySelectorAll('input[name="skills"]:checked');
-    var city= document.getElementById("city").value;
+//     var fname= document.getElementById("fname").value;
+//      var lname= document.getElementById("lname").value;
+//      var email= document.getElementById("email").value;
+//      var pass= document.getElementById("pass").value;
+//      var gender=document.querySelector('input[name="gen"]:checked');
+//     var skills=document.querySelectorAll('input[name="skills"]:checked');
+//     var city= document.getElementById("city").value;
 
-if(fname==""){
-    document.getElementById("fnameError").innerHTML="Enter the  name";
-    return false;
+// if(fname==""){
+//     document.getElementById("fnameError").innerHTML="Enter the  name";
+//     return false;
 
-}
-if(lname==""){
-    document.getElementById("lnameError").innerHTML="Enter the  last name";
-    return false;
+// }
+// if(lname==""){
+//     document.getElementById("lnameError").innerHTML="Enter the  last name";
+//     return false;
     
-}
-if(email==""){
-    document.getElementById("emailError").innerHTML="Enter the  email";
-    return false;
-}
-    if(pass==""){
-    document.getElementById("passError").innerHTML="Enter the  password";
-    return false;
-    }
-if(!gender==""){
-    document.getElementById("genderError").innerHTML="Enter the  gender";
-    return false;
+// }
+// if(email==""){
+//     document.getElementById("emailError").innerHTML="Enter the  email";
+//     return false;
+// }
+//     if(pass==""){
+//     document.getElementById("passError").innerHTML="Enter the  password";
+//     return false;
+//     }
+// if(!gender==""){
+//     document.getElementById("genderError").innerHTML="Enter the  gender";
+//     return false;
     
-}
-if(skills.length==0){
-    document.getElementById("skillsError").innerHTML="select your city";
-    return false;
+// }
+// if(skills.length==0){
+//     document.getElementById("skillsError").innerHTML="select your city";
+//     return false;
     
-}
-if(city==""){
-    document.getElementById("cityError").innerHTML="select your city";
-    return false;
+// }
+// if(city==""){
+//     document.getElementById("cityError").innerHTML="select your city";
+//     return false;
     
-}
-return true;
+// }
+// return true;
 
-}
+// }
